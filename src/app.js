b427a2386f99557b2370f7702d1dba797f5c7802
Davidgmzc <davidgomz.c13@gmail.com>
2024-11-28 16:30:40 +0100

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,35 @@
+/* eslint-disable */
+import "bootstrap";
+import "./style.css";
+
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+
+window.onload = function() {
+  //write your code here
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function getRandomElement(list) {
+    let element = list[Math.floor(Math.random() * list.length)];
+    return element;
+  }
+  let excuse = document.querySelector("#excuse");
+
+  excuse.innerHTML =
+    getRandomElement(who) +
+    " " +
+    getRandomElement(action) +
+    " " +
+    getRandomElement(what) +
+    " " +
+    getRandomElement(when);
+};
